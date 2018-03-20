ac521b31c7d39cc597c83ab55c92c14b2c33d3e5
Majd Murad <murad.majd@gmail.com>
2018-03-20 17:02:50 -0400

    Fix animating expanding area

--- app/js/components/ExpandingArea.jsx
+++ app/js/components/ExpandingArea.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+import {ReactHeight} from 'react-height';
+
 import styles from '../../styles/Item.scss';
 
-const ExpandingArea = ({children, isSelected, onSelect, title}) => {
-    const expandingAreaClasses = classnames(styles.expandingArea, {
-        [styles.expanded]: isSelected,
-    });
-    const arrowClasses = classnames(styles.arrow, {
-        [styles.arrowExpanded]: isSelected,
-    })
-    return (
-        <div className={styles.expandingAreaWrapper} onClick={onSelect}>
-            <div className={styles.expandingAreaTitle}><img className={arrowClasses} src="../../assets/arrow-collapsed.png" />{title}</div>
-            <div className={expandingAreaClasses}>{children}</div>
-        </div>
-    );
+class ExpandingArea extends Component {
+    constructor(props) {
+        super(props);
+
+        this.updateHeight = this.updateHeight.bind(this);
+        this.state = {
+            height: 150,
+        };
+    }
+    updateHeight(height) {
+        this.setState({height});
+    }
+    render() {
+        const {children, isSelected, onSelect, title} = this.props;
+        const expandingAreaClasses = classnames(styles.expandingArea, {
+            [styles.expanded]: isSelected,
+        });
+        const arrowClasses = classnames(styles.arrow, {
+            [styles.arrowExpanded]: isSelected,
+        })
+        const {height} = this.state;
+        const maxHeight = isSelected ? height : 0;
+        
+        return (
+            <div className={styles.expandingAreaWrapper} onClick={onSelect}>
+                <div className={styles.expandingAreaTitle}><img className={arrowClasses} src="../../assets/arrow-collapsed.png" />{title}</div>
+                <div className={expandingAreaClasses} style={{maxHeight}}>
+                    <ReactHeight onHeightReady={this.updateHeight}>
+                        <div>{children}</div>
+                    </ReactHeight>
+                </div>
+            </div>
+        );
+        
+    }
 }
 
 ExpandingArea.propTypes = {
